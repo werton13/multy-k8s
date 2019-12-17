dd0f140fbee578897995d9dfe4d723f82622416f
Ubuntu <aksadmin@AKSEngine.c1838c64-2bb5-409e-8ae7-74c852cd366b.internal.azs.mtscloud.local>
2019-12-17 06:03:23 +0000

    trying to find out current directory to copy crt files

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ docker push werton13/multi-worker:latest
 docker push werton13/multi-client:$GIT_SHA
 docker push werton13/multi-server:$GIT_SHA
 docker push werton13/multi-worker:$GIT_SHA
+echo $PWD
 kubectl apply -f K8s
 kubectl set image  deployments/server-deployment server=werton13/multi-server:$GIT_SHA
 kubectl set image  deployments/client-deployment client=werton13/multi-client:$GIT_SHA
